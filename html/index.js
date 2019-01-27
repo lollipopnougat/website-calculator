6dad5ae724eb6eba0aed50df7dfba98de2925268
LollipopNougat <33110314+lollipopnougat@users.noreply.github.com>
2019-01-28 00:22:33 +0800

    add

---
+++ html/index.js
@@ -0,0 +1,10 @@
+
+
+function hou() {
+        let hh = confirm("支持一下作者博客？");
+        if (hh) window.open("https://crepe.coding.me/Crepeblog/");
+        else alert("emmm……");
+        hh = confirm("关注一下作者Github？");
+        if (hh) window.open("https://github.com/lollipopnougat");
+        else alert("你这样子是不行的！");
+    }
